d12cd01524616b5a5df7fcde1236bdbc61197cc5
JimmyMath <37000638+JimmyMath@users.noreply.github.com>
2018-03-23 19:33:46 +0100

    Add files via upload

---
+++ Arrays/firstDuplicate.py
@@ -0,0 +1,23 @@
+'''
+Created on Mar 14, 2018
+
+@author: Jim
+'''
+def firstDuplicate(a):
+    l = zip(a,range(len(a)))
+    di = list_to_dict(l)
+    ll = [(v[1],k) for k, v in di.iteritems() if len(v) > 1]
+    if not ll:
+        return -1
+    else:
+        (x,y) = min(ll)
+        return y
+
+def list_to_dict(li):
+     dct = {}
+     for (item,v) in li:
+         if dct.has_key(item):
+                 dct[item] = dct[item] + [v]
+         else:
+             dct[item] = [v]
+     return dct
