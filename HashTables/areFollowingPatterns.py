24945374baddbea2d449e1d6c7bb66062a511d81
JimmyMath <37000638+JimmyMath@users.noreply.github.com>
2018-03-27 23:45:58 +0200

    Update areFollowingPatterns.py

--- HashTables/areFollowingPatterns.py
+++ HashTables/areFollowingPatterns.py
@@ -15,7 +15,7 @@
 #An array of strings, each containing only lowercase English letters.
 
 #Guaranteed constraints:
-#1 ≤ strings.length ≤ 105,
+#1 ≤ strings.length ≤ 10**5,
 #1 ≤ strings[i].length ≤ 10.
 
 #[input] array.string patterns
