0e75899bac5e0a5e74dd548a69689876a4926cd8
JimmyMath <37000638+JimmyMath@users.noreply.github.com>
2018-03-27 23:24:45 +0200

    Update containsDuplicates.py

--- CommonTechniques:Basic/containsDuplicates.py
+++ CommonTechniques:Basic/containsDuplicates.py
@@ -1,3 +1,29 @@
+#Given an array of integers, write a function that determines whether the array contains any duplicates. Your function should return true if any element appears at least twice in the array, and it should return false if every element is distinct.
+
+#Example
+
+#For a = [1, 2, 3, 1], the output should be
+#containsDuplicates(a) = true.
+
+#There are two 1s in the given array.
+
+#For a = [3, 1], the output should be
+#containsDuplicates(a) = false.
+
+#The given array contains no duplicates.
+
+#Input/Output
+
+#[execution time limit] 4 seconds (py)
+
+#[input] array.integer a
+
+#Guaranteed constraints:
+#0 ≤ a.length ≤ 10**5,
+#-2 * 10**9 ≤ a[i] ≤ 2 * 10**9.
+
+#[output] boolean
+
 def containsDuplicates(a):
     D={}
     for i in range(len(a)):
