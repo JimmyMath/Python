4775b6fe441c7de47b80cfd24e7fef52c9d42da4
JimmyMath <37000638+JimmyMath@users.noreply.github.com>
2018-03-27 23:33:51 +0200

    Update arrayMaxConsecutiveSum2.py

--- CommonTechniques:Basic/arrayMaxConsecutiveSum2.py
+++ CommonTechniques:Basic/arrayMaxConsecutiveSum2.py
@@ -1,3 +1,28 @@
+#Given an array of integers, find the maximum possible sum you can get from one of its contiguous subarrays. The subarray from which this sum comes must contain at least 1 element.
+
+#Example
+
+#For inputArray = [-2, 2, 5, -11, 6], the output should be
+#arrayMaxConsecutiveSum2(inputArray) = 7.
+
+#The contiguous subarray that gives the maximum possible sum is [2, 5], with a sum of 7.
+
+#Input/Output
+
+#[execution time limit] 4 seconds (py)
+
+#[input] array.integer inputArray
+
+#An array of integers.
+
+#Guaranteed constraints:
+#3 ≤ inputArray.length ≤ 10**5,
+#-1000 ≤ inputArray[i] ≤ 1000.
+
+#[output] integer
+
+#The maximum possible sum of a subarray within inputArray.
+
 def arrayMaxConsecutiveSum2(inputArray):
     currmax,m=inputArray[0],inputArray[0]
     for i in range(1,len(inputArray)):
