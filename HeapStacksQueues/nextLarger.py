5a87f8df021774981f4ff1000f413834370229cc
JimmyMath <37000638+JimmyMath@users.noreply.github.com>
2018-03-28 08:31:56 +0200

    Update nextLarger.py

--- HeapStacksQueues/nextLarger.py
+++ HeapStacksQueues/nextLarger.py
@@ -26,19 +26,18 @@
 #The result array, in which the ith cell corresponds to the first element in a to the right of a[i] that is larger than a[i], or -1 if there is no such element.
 
 def nextLarger(a):
-    a=a[::-1]
-    Larger=[-1]
-    
+    a = a[::-1]
+    Larger = [-1]
     for i in range(len(a)-1):
-        if a[i]>a[i+1]:
+        if a[i] > a[i+1]:
             Larger.append(a[i]) 
         else:
-            b=a[:i]
-            while b!=[]:
-                if b[-1]>a[i+1]:
+            b = a[:i]
+            while b != []:
+                if b[-1] > a[i+1]:
                     Larger.append(b[-1])
                     break
                 b.pop()
-            if b==[]:
+            if b == []:
                 Larger.append(-1)
     return Larger[::-1]
