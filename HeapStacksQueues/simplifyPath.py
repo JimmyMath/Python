fabc18e04a20f051cb9ee967cc7706c3bbb6595f
JimmyMath <37000638+JimmyMath@users.noreply.github.com>
2018-03-28 08:33:22 +0200

    Update simplifyPath.py

--- HeapStacksQueues/simplifyPath.py
+++ HeapStacksQueues/simplifyPath.py
@@ -34,24 +34,24 @@
 
 import heapq
 def simplifyPath(path):
-    ps=path.split("/")
-    array=[]
+    ps = path.split("/")
+    array = []
     for x in ps:
-        if x!='' and x!='.':
+        if x != '' and x != '.':
             array.append(x)
-    h=[]
+    h = []
     for i in range(len(array)):
-        if array[i]!="..":
-            heapq.heappush(h,(len(array)-1-i,array[i]))
+        if array[i] != "..":
+            heapq.heappush( h, (len(array)-1-i, array[i]) )
         else:
-            heapq.heappushpop(h,(len(array),array[i]))
-    array1=[]
+            heapq.heappushpop( h, (len(array), array[i]) )
+    array1 = []
     for x in h:
-        if x[1]!="..":
+        if x[1] != "..":
             array1.append(x)
-    if array1==[]:
+    if array1 == []:
         return "/"
-    s=""
+    s = ""
     for y in sorted(array1)[::-1]:
-        s+="/"+y[1]
+        s += "/" + y[1]
     return s
