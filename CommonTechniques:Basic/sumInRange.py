974d5491a5c4906193c8c93d408ef19b8ea53da8
JimmyMath <37000638+JimmyMath@users.noreply.github.com>
2018-03-28 08:20:22 +0200

    Update sumInRange.py

--- CommonTechniques:Basic/sumInRange.py
+++ CommonTechniques:Basic/sumInRange.py
@@ -35,10 +35,10 @@
 
 import numpy as np
 def sumInRange(nums, queries):
-    N,S = np.cumsum(nums),0
+    N, S = np.cumsum(nums), 0
     for i in range(len(queries)):
-        if queries[i][0]==0:
-            S+=N[queries[i][1]]
+        if queries[i][0] == 0:
+            S += N[queries[i][1]]
         else:
-            S+=N[queries[i][1]]-N[queries[i][0]-1]
-    return S%(10**9+7)
+            S += N[queries[i][1]] - N[queries[i][0]-1]
+    return S % (10**9 + 7)
