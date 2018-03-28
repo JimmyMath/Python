be8302a8cbb9ab40ba79939286e9b5eb660b6537
JimmyMath <37000638+JimmyMath@users.noreply.github.com>
2018-03-28 08:19:33 +0200

    Update findLongestSubarrayBySum.py

--- CommonTechniques:Basic/findLongestSubarrayBySum.py
+++ CommonTechniques:Basic/findLongestSubarrayBySum.py
@@ -44,16 +44,16 @@
 
 import numpy as np
 def findLongestSubarrayBySum(s, arr):
-    N,index,m=np.cumsum([0]+arr),[],0
+    N, index, m = np.cumsum([0]+arr), [], 0
     for i in range(len(arr)):
         index.append(i+1)
-        if N[index[-1]]-N[index[0]-1]==s and len(index)>m:
-            m,longest=len(index),[index[0],index[-1]]
-        if N[index[-1]]-N[index[0]-1]>s:
-            while index!=[] and N[index[-1]]-N[index[0]-1]>s:
+        if N[index[-1]] - N[index[0]-1] == s and len(index) > m:
+            m, longest = len(index), [index[0], index[-1]]
+        if N[index[-1]] - N[index[0]-1] > s:
+            while index != [] and N[index[-1]] - N[index[0]-1] > s:
                 del index[0]
-            if index!=[] and N[index[-1]]-N[index[0]-1]==s and len(index)>m:
-                m,longest=len(index),[index[0],index[-1]]
-    if m==0:
+            if index != [] and N[index[-1]] - N[index[0]-1] == s and len(index) > m:
+                m, longest = len(index), [index[0], index[-1]]
+    if m == 0:
         return [-1]
     return longest
