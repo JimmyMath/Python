045b5e0b38c5fbe73c350793c1120406e7902880
JimmyMath <37000638+JimmyMath@users.noreply.github.com>
2018-03-28 08:26:43 +0200

    Update containsCloseNums.py

--- HashTables/containsCloseNums.py
+++ HashTables/containsCloseNums.py
@@ -31,17 +31,17 @@
 #[output] boolean
 
 def containsCloseNums(nums, k):
-    D={}
+    D = {}
     for i in range(len(nums)):
         if nums[i] in D:
-            D[nums[i]]=D[nums[i]]+[i]
+            D[nums[i]] = D[nums[i]] + [i]
         else:
-            D[nums[i]]=[i]
+            D[nums[i]] = [i] 
  
     for v in D.values():
-        if len(v)>1:
+        if len(v) > 1:
             for x in v:
                 for y in v:
-                    if abs(x-y)<=k and x!=y:
+                    if abs(x-y) <= k and x != y:
                         return True
     return False
