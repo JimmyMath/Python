865ae4e04b572add378929faf87f829a59eca1eb
JimmyMath <37000638+JimmyMath@users.noreply.github.com>
2018-03-28 08:14:39 +0200

    Update rotateImage.py

--- Arrays/rotateImage.py
+++ Arrays/rotateImage.py
@@ -30,9 +30,9 @@
 #[output] array.array.integer
 
 def rotateImage(a):
-    k=len(a)
+    k = len(a)
     Matrix = [[0 for x in range(0,k)] for y in range(0,k)]
     for i in range(0,k):
         for j in range(0,k):
-            Matrix[i][j]=a[k-1-j][i]
+            Matrix[i][j] = a[k-1-j][i]
     return Matrix
