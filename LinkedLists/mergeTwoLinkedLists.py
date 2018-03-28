7744814533fee2d0778bc47c3d22deceaca0fbc2
JimmyMath <37000638+JimmyMath@users.noreply.github.com>
2018-03-28 08:34:23 +0200

    Update mergeTwoLinkedLists.py

--- LinkedLists/mergeTwoLinkedLists.py
+++ LinkedLists/mergeTwoLinkedLists.py
@@ -39,23 +39,23 @@
 #     self.next = None
 #
 def mergeTwoLinkedLists(l1, l2):
-    if l1==None or l2==None:
+    if l1 == None or l2 == None:
         return l1 or l2
-    dummy=ListNode(None)
-    curr=dummy
+    dummy = ListNode(None)
+    curr = dummy
     while True:
-        if l1.value<l2.value:
-            curr.next=l1
-            curr=curr.next
-            if l1.next==None:
-                curr.next=l2
+        if l1.value < l2.value:
+            curr.next = l1
+            curr = curr.next
+            if l1.next == None:
+                curr.next = l2
                 break
-            l1=l1.next
+            l1 = l1.next
         else:
-            curr.next=l2
-            curr=curr.next
-            if l2.next==None:
-                curr.next=l1
+            curr.next = l2
+            curr = curr.next
+            if l2.next == None:
+                curr.next = l1
                 break
-            l2=l2.next   
+            l2 = l2.next   
     return dummy.next
