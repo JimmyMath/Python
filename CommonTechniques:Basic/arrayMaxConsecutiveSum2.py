bd6dc97e77bcc7d9abc8b474581a2ba6982e8fb1
JimmyMath <37000638+JimmyMath@users.noreply.github.com>
2018-03-28 08:17:34 +0200

    Update arrayMaxConsecutiveSum2.py

--- CommonTechniques:Basic/arrayMaxConsecutiveSum2.py
+++ CommonTechniques:Basic/arrayMaxConsecutiveSum2.py
@@ -24,8 +24,8 @@
 #The maximum possible sum of a subarray within inputArray.
 
 def arrayMaxConsecutiveSum2(inputArray):
-    currmax,m=inputArray[0],inputArray[0]
-    for i in range(1,len(inputArray)):
-        currmax=max(inputArray[i],currmax+inputArray[i])
-        m=max(currmax,m)
+    currmax, m = inputArray[0], inputArray[0]
+    for i in range(1, len(inputArray)):
+        currmax = max(inputArray[i], currmax + inputArray[i])
+        m = max(currmax, m)
     return m
