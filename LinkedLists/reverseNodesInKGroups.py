edfdec059b99d52d3ad184141514cf816b32cb88
JimmyMath <37000638+JimmyMath@users.noreply.github.com>
2018-03-28 08:36:51 +0200

    Update reverseNodesInKGroups.py

--- LinkedLists/reverseNodesInKGroups.py
+++ LinkedLists/reverseNodesInKGroups.py
@@ -46,35 +46,25 @@
 def reverseNodesInKGroups(l, k):
     if k == 1:
         return l
-    
     r,i = l.next,1
-    
     while r:
         r = r.next
         i = i+1
-    
     if i < k:
         return l
-    
     dummy, n = ListNode(None), None
-    
     for j in range(k):
         m = l.next
         l.next = n
         n = l
         dummy.next = l
         l = m
-    
-    if i==k:
+    if i == k:
         return dummy.next
-    
-    s=dummy.next.next
-    
+    s = dummy.next.next
     for a in range(k-2):
         s=s.next
-    
     s.next=reverseNodesInKGroups(l, k)
-    
     return dummy.next
         
         
