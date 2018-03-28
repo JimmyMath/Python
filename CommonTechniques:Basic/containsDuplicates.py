266293d8401a77ce5ad2360a06e72c6b32f5c674
JimmyMath <37000638+JimmyMath@users.noreply.github.com>
2018-03-28 08:09:57 +0200

    Update containsDuplicates.py

--- CommonTechniques:Basic/containsDuplicates.py
+++ CommonTechniques:Basic/containsDuplicates.py
@@ -25,13 +25,5 @@
 #[output] boolean
 
 def containsDuplicates(a):
-    D={}
-    for i in range(len(a)):
-        if a[i] not in D:
-            D[a[i]]=[i]
-        else:
-            D[a[i]]=D[a[i]]+[i]
-    for k in D.keys():
-        if len(D[k])>1:
-            return True
-    return False
+    return len(a)!=len(set(a))
+    
