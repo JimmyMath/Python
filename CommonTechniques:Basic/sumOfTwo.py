ac2dc77d76589d61d012f9d27d44ebb65dfe4fd9
JimmyMath <37000638+JimmyMath@users.noreply.github.com>
2018-03-28 08:20:49 +0200

    Update sumOfTwo.py

--- CommonTechniques:Basic/sumOfTwo.py
+++ CommonTechniques:Basic/sumOfTwo.py
@@ -35,9 +35,9 @@
 #true if there are two elements from a and b which add up to v, and false otherwise.
 
 def sumOfTwo(a, b, v):
-    S=set(a)
-    b=list(set(b))
+    S = set(a)
+    b = list(set(b))
     for x in b:
-        if v-x in S:
+        if v - x in S:
             return True
     return False
