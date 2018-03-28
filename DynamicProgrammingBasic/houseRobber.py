eeae78c8a34014394191ef856d775dc8c2cb98e3
JimmyMath <37000638+JimmyMath@users.noreply.github.com>
2018-03-28 08:25:11 +0200

    Update houseRobber.py

--- DynamicProgrammingBasic/houseRobber.py
+++ DynamicProgrammingBasic/houseRobber.py
@@ -28,10 +28,10 @@ dict = Counter()
 def houseRobber(nums):
     if dict[len(nums)] != 0:
         return dict[len(nums)]
-    if len(nums)==0:
+    if len(nums) == 0:
         return 0
-    elif len(nums)==1:
+    elif len(nums) == 1:
         return nums[0]
     else:
-        dict[len(nums[1:])]= houseRobber(nums[1:])
+        dict[len(nums[1:])] = houseRobber(nums[1:])
         return max(dict[len(nums[1:])], nums[0]+dict[len(nums[2:])])
