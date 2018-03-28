bf8adb104fc7fdee67287cc1a13638772416cc1a
JimmyMath <37000638+JimmyMath@users.noreply.github.com>
2018-03-28 08:30:13 +0200

    Update countClouds.py

--- HeapStacksQueues/countClouds.py
+++ HeapStacksQueues/countClouds.py
@@ -39,21 +39,21 @@
 #The number of clouds in the given skyMap, as described above.
 
 def countClouds(skyMap):
-    n=0
+    n = 0
     for i in range(len(skyMap)):
         for j in range(len(skyMap[0])):
             if skyMap[i][j] == '1':
-                n+=1
-                modifyClouds(skyMap,i,j)
+                n += 1
+                modifyClouds(skyMap, i, j)
     return n
                 
-def modifyClouds(M,k,l):
-    if 0<=k<len(M) and 0<=l<len(M[0]) and M[k][l]=='1':
-        M[k][l]='0'
-        modifyClouds(M,k-1,l)
-        modifyClouds(M,k+1,l)
-        modifyClouds(M,k,l-1)
-        modifyClouds(M,k,l+1)
+def modifyClouds(M, k, l):
+    if 0 <= k < len(M) and 0 <= l < len(M[0]) and M[k][l] == '1':
+        M[k][l] = '0'
+        modifyClouds(M, k-1, l)
+        modifyClouds(M, k+1, l)
+        modifyClouds(M, k, l-1)
+        modifyClouds(M, k, l+1)
     else: 
         return
     
