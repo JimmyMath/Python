2bf5bbea21516fe8e02b2762881aab9238e105b9
JimmyMath <37000638+JimmyMath@users.noreply.github.com>
2018-03-28 08:24:22 +0200

    Update composeRanges.py

--- DynamicProgrammingBasic/composeRanges.py
+++ DynamicProgrammingBasic/composeRanges.py
@@ -20,18 +20,18 @@
 #[output] array.string
 
 def composeRanges(nums):
-    if len(nums)==0:
+    if len(nums) == 0:
         return []
-    elif len(nums)==1:
+    elif len(nums) == 1:
         return [str(nums[0])]
     else:
-        for i in range(0,len(nums)-1):
-            if nums[i]+1<nums[i+1]:
-                if i==0:
-                    array=[str(nums[0])]
+        for i in range(0, len(nums)-1):
+            if nums[i] + 1 < nums[i+1]:
+                if i == 0:
+                    array = [str(nums[0])]
                 else:
-                    array=[str(nums[0])+"->"+str(nums[i])]
-                return array+composeRanges(nums[i+1:])
+                    array=[str(nums[0]) + "->" + str(nums[i])]
+                return array + composeRanges(nums[i+1:])
             else:
-                if i==len(nums)-2:
-                    return [str(nums[0])+"->"+str(nums[-1])]
+                if i == len(nums)-2:
+                    return [str(nums[0]) + "->" + str(nums[-1])]
