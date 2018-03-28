8a7aba9bbe0438e04b16ec4d23cf339fb416b2a1
JimmyMath <37000638+JimmyMath@users.noreply.github.com>
2018-03-28 08:25:57 +0200

    Update areFollowingPatterns.py

--- HashTables/areFollowingPatterns.py
+++ HashTables/areFollowingPatterns.py
@@ -31,21 +31,21 @@
 #Return true if strings follows patterns and false otherwise.
 
 def areFollowingPatterns(strings, patterns):
-    shash,phash={},{}
+    shash, phash={}, {}
     for i in range(len(strings)):
         if strings[i] in shash:
-            shash[strings[i]]=shash[strings[i]]+[i]
+            shash[strings[i]] = shash[strings[i]]+[i]
         else:
-            shash[strings[i]]=[i]
+            shash[strings[i]] = [i]
     
     for j in range(len(patterns)):
         if patterns[j] in phash:
-            phash[patterns[j]]=phash[patterns[j]]+[j]
+            phash[patterns[j]] = phash[patterns[j]]+[j]
         else:
-            phash[patterns[j]]=[j]
+            phash[patterns[j]] = [j]
     
     for v in shash.values():
         for w in phash.values():
-            if (len(v)>1 and v not in phash.values()) or (len(w)>1 and w not in shash.values()):
+            if (len(v) > 1 and v not in phash.values()) or (len(w) > 1 and w not in shash.values()):
                     return False
     return True
