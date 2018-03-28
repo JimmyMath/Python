23d05ac8c1caf3f48307c1cfb9919b070530e6e7
JimmyMath <37000638+JimmyMath@users.noreply.github.com>
2018-03-28 08:28:14 +0200

    Update groupingDishes.py

--- HashTables/groupingDishes.py
+++ HashTables/groupingDishes.py
@@ -45,19 +45,16 @@
 
 
 def groupingDishes(dishes):
-    keys,D=[],{}
+    keys, D = [], {}
     for row in dishes:
-        for i in range(1,len(row)):
-            keys=keys+[row[i]]
-    
-    keys=sorted([x for i, x in enumerate(keys) if x not in keys[:i]])
-    
+        for i in range(1, len(row)):
+            keys += [row[i]]
+    keys = sorted([x for i, x in enumerate(keys) if x not in keys[:i]])
     for k in keys:
-        values=[]
+        values = []
         for row in dishes:
             if k in row[1:]:
-                values=values+[row[0]]
-        D[k]=sorted(values)
-
-    return [[k]+D[k] for k in keys if len(hash[k]) > 1]
+                values += [row[0]]
+        D[k] = sorted(values)
+    return [[k] + D[k] for k in keys if len(hash[k]) > 1]
     
