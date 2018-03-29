a1558bad92025e3bebbcb8923cb8ccae2862d567
JimmyMath <37000638+JimmyMath@users.noreply.github.com>
2018-03-29 18:09:51 +0200

    Update minSubstringWithAllChars.py

--- CommonTechniques:Basic/minSubstringWithAllChars.py
+++ CommonTechniques:Basic/minSubstringWithAllChars.py
@@ -1,4 +1,4 @@
-#You have two strings, s and t. The string t contains only unique elements. Find and return the minimum consecutive substring of s that contains all of the elements from t.
+ #You have two strings, s and t. The string t contains only unique elements. Find and return the minimum consecutive substring of s that contains all of the elements from t.
 
 #It's guaranteed that the answer exists. If there are several answers, return the one which starts from the smallest index.
 
