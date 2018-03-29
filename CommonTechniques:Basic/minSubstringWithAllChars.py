79cd08451653741c0c789e4a8210416b4f19f275
JimmyMath <37000638+JimmyMath@users.noreply.github.com>
2018-03-29 17:58:53 +0200

    Update minSubstringWithAllChars.py

--- CommonTechniques:Basic/minSubstringWithAllChars.py
+++ CommonTechniques:Basic/minSubstringWithAllChars.py
@@ -1,3 +1,32 @@
+#You have two strings, s and t. The string t contains only unique elements. Find and return the minimum consecutive substring of s that contains all of the elements from t.
+
+#It's guaranteed that the answer exists. If there are several answers, return the one which starts from the smallest index.
+
+#Example
+
+#For s = "adobecodebanc" and t = "abc", the output should be
+#minSubstringWithAllChars(s, t) = "banc".
+
+#Input/Output
+
+#[execution time limit] 4 seconds (py)
+
+#[input] string s
+
+#A string consisting only of lowercase English letters.
+
+#Guaranteed constraints:
+#0 ≤ s.length ≤ 100.
+
+#[input] string t
+
+#A string consisting only of unique lowercase English letters.
+
+#Guaranteed constraints:
+#0 ≤ t.length ≤ min(26, s.length).
+
+#[output] string
+
 def minSubstringWithAllChars(s, t):
     if t == "":return ""
     S = list(s)
