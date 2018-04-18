939c3246251a531b6b5c429d6c7a3edddbef57cd
JimmyMath <37000638+JimmyMath@users.noreply.github.com>
2018-04-18 16:43:04 +0200

    Update longestIncreasingSubsequence.py

--- DynamicProgrammingAdvanced/longestIncreasingSubsequence.py
+++ DynamicProgrammingAdvanced/longestIncreasingSubsequence.py
@@ -22,6 +22,22 @@
 #1 ≤ sequence.length ≤ 1000,
 
 #0 ≤ sequence[i] ≤ 10**6. 
+
+#[output] integer
+
+#The length of the longest increasing subsequence of a given sequence.
+
+def longestIncreasingSubsequence(sequence):
+    k = len(sequence)
+    s = [(0,1)]
+    for i in range(1,k):
+        m = 1
+        for j in range(i-1,-1,-1):
+            if sequence[j] < sequence[i]:
+                if m < s[j][1]+1:
+                    m = s[j][1]+1
+        if m > 1:
+            s += [(i,m)]
         else:
             s += [(i,1)]
     return max([s[i][1] for i in range(k)])
