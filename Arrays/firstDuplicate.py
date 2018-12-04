b4aa7c656c14e52e0a5a139c6c3f5e6b1b53775a
JimmyMath <37000638+JimmyMath@users.noreply.github.com>
2018-12-05 07:25:01 +0900

    Update firstDuplicate.py

--- Arrays/firstDuplicate.py
+++ Arrays/firstDuplicate.py
@@ -4,8 +4,7 @@
 
 #Example
 
-#For a = [2, 3, 3, 1, 5, 2], the output should be
-#firstDuplicate(a) = 3.
+#For a = [2, 3, 3, 1, 5, 2], the output should be firstDuplicate(a) = 3.
 
 #There are 2 duplicates: numbers 2 and 3. The second occurrence of 3 has a smaller index than than second occurrence of 2 does, so the answer is 3.
 
