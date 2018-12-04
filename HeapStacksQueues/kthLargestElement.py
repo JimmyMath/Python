46ac03e22a7f993cbf722c2d939e32fff9fa3e8f
JimmyMath <37000638+JimmyMath@users.noreply.github.com>
2018-12-05 07:45:58 +0900

    Update kthLargestElement.py

--- HeapStacksQueues/kthLargestElement.py
+++ HeapStacksQueues/kthLargestElement.py
@@ -4,10 +4,8 @@
 
 #Example
 
-#For nums = [7, 6, 5, 4, 3, 2, 1] and k = 2, the output should be
-#kthLargestElement(nums, k) = 6;
-#For nums = [99, 99] and k = 1, the output should be
-#kthLargestElement(nums, k) = 99.
+#For nums = [7, 6, 5, 4, 3, 2, 1] and k = 2, the output should be kthLargestElement(nums, k) = 6;
+#For nums = [99, 99] and k = 1, the output should be kthLargestElement(nums, k) = 99.
 #Input/Output
 
 #[execution time limit] 4 seconds (py)
