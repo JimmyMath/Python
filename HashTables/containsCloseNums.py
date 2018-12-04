311c7347d7b2023dcfcb86fbc881a4b2ec38f4f3
JimmyMath <37000638+JimmyMath@users.noreply.github.com>
2018-12-05 07:31:26 +0900

    Update containsCloseNums.py

--- HashTables/containsCloseNums.py
+++ HashTables/containsCloseNums.py
@@ -7,9 +7,7 @@
 
 #There are two 2s in nums, and the absolute difference between their positions is exactly 3.
 
-#For nums = [0, 1, 2, 3, 5, 2] and k = 2, the output should be
-
-#containsCloseNums(nums, k) = false.
+#For nums = [0, 1, 2, 3, 5, 2] and k = 2, the output should be containsCloseNums(nums, k) = false.
 
 #The absolute difference between the positions of the two 2s is 3, which is more than k.
 
