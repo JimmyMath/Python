6cc01e36137d0d9bea4c919fd8929d012febfd8a
JimmyMath <37000638+JimmyMath@users.noreply.github.com>
2018-12-05 07:06:49 +0900

    Update treeBottom.py

--- Challenge/treeBottom.py
+++ Challenge/treeBottom.py
@@ -1,8 +1,37 @@
-'''
-Created on Dec 5, 2018
+#You are given a recursive notation of a binary tree: each node of a tree is represented as a set of three elements:
 
-@author: Jim
-'''
+#value of the node;
+#left subtree;
+#right subtree.
+#So, a tree can be written as (value left_subtree right_subtree). It is guaranteed that 1 ≤ value ≤ 109. If a node doesn't exist then it is represented as an empty set: (). For example, here is a representation of a tree in the given picture:
+
+#(2 (7 (2 () ()) (6 (5 () ()) (11 () ()))) (5 () (9 (4 () ()) ())))
+
+#Your task is to obtain a list of nodes, that are the most distant from the tree root, in the order from left to right.
+
+#In the notation of a node its value and subtrees are separated by exactly one space character.
+
+#Example
+
+#For
+
+#tree = "(2 (7 (2 () ()) (6 (5 () ()) (11 () ()))) (5 () (9 (4 () ()) ())))"
+#the output should be
+#treeBottom(tree) = [5, 11, 4].
+
+#Input/Output
+
+#[execution time limit] 4 seconds (py)
+
+#[input] string tree
+
+#Guaranteed constraints:
+#5 ≤ tree.length ≤ 120.
+
+#[output] array.integer
+
+#[Python2] Syntax Tips
+
 def treeBottom(tree):
     x , y, k = 0, 0, len(tree)
     for j in range(1,k):
